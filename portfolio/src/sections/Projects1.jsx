8ae36eea5f49b59d8867ec889aef5ae337af3e38
William Chan <chan.william.ts@gmail.com>
2023-12-31 17:44:26 -0500

    card component created

--- portfolio/src/sections/Projects1.jsx
+++ portfolio/src/sections/Projects1.jsx
@@ -2,6 +2,7 @@ import styles from './Projects1.module.css';
 import about from '../img/about.jpg';
 import { VscGithubAlt } from 'react-icons/vsc';
 import { IoIosGlobe } from 'react-icons/io';
+import Card from '../components/Card';
 
 function Projects1() {
   return (
@@ -12,43 +13,8 @@ function Projects1() {
           Built from scratch and from the bottom of my ❤️
         </h3>
 
-        <div className={styles.card}>
-          <div className={styles.imageContainer}>
-            <img className={styles.image} src={about} alt="dog" />
-          </div>
-
-          <div className={styles.textContainer}>
-            <h3 className={styles.title}>Project #1</h3>
-            <p className={styles.text}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Temporibus animi dolor impedit, praesentium esse nisi Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Temporibus animi
-              dolor impedit, praesentium esse nisi
-            </p>
-
-            <div className={styles.languages}>
-              <div className={styles.language}>
-                <p>React</p>
-              </div>
-              <div className={styles.language}>
-                <p>CSS</p>
-              </div>
-            </div>
-
-            <div className={styles.links}>
-              <div className={styles.link}>
-                <a href="#">
-                  Code <VscGithubAlt className={styles.emoji} />
-                </a>
-              </div>
-              <div className={styles.link}>
-                <a href="#">
-                  Demo <IoIosGlobe className={styles.emoji} />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Card />
+        <Card />
       </div>
     </section>
   );
