edd8032fea8d514c3d43a2c57de74e891fd63696
William Chan <chan.william.ts@gmail.com>
2024-01-02 19:39:55 -0500

    creating dropdown component for media width < 600px

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,26 +1,53 @@
 import { HashLink as Link } from 'react-router-hash-link';
 import styles from './NavBar.module.css';
+import { IoReorderThree } from 'react-icons/io5';
+import { useEffect, useState } from 'react';
+{
+  /* <IoReorderThree /> */
+}
 
 function NavBar() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(function () {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
       <div className={styles.container}>
         <h1 className={styles.logo}>Chan</h1>
-        <div className={styles.links}>
-          <Link to="#home" smooth>
-            <p>Home</p>
-          </Link>
-          <Link to="#about" smooth>
-            <p>About</p>
-          </Link>
-          <Link to="#projects" smooth>
-            <p>Projects</p>
-          </Link>
-          <Link to="#contact" smooth>
-            <p>Contact</p>
-          </Link>
+        {/* {window.innerWidth < 600 ? (
+          <div>
+            <IoReorderThree />
+          </div>
+        ) : (
+          <div className={styles.links}>
+            <Link to="#home" smooth>
+              <p>Home</p>
+            </Link>
+            <Link to="#about" smooth>
+              <p>About</p>
+            </Link>
+            <Link to="#projects" smooth>
+              <p>Projects</p>
+            </Link>
+            <Link to="#contact" smooth>
+              <p>Contact</p>
+            </Link>
+          </div>
+        )} */}
+        <div>
+          <IoReorderThree />
         </div>
-        {/* </div> */}
       </div>
     </>
   );
